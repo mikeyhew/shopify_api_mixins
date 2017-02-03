e79231e0be43c0cafed2d89fd1ba91968c0744ec
Michael Hewson <michael@michaelhewson.ca>
2017-02-03 01:39:22 -0500

    add test stuff

---
+++ test/shopify_api_mixins_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ShopifyApiMixinsTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ShopifyApiMixins::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'shopify_api_mixins'
+
+require 'minitest/autorun'
