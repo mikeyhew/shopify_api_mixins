9d3ec9433fbb5f3902cfa933f5792a048449b1a3
Michael Hewson <michael@michaelhewson.ca>
2017-03-26 17:05:45 -0400

    Update dependencies to the minimum required
    activeresource hasn’t had any  version bumps since I added the
    connection_class attribute. So I added a check to see if it is
    available, which raises an error if it isn’t.

--- lib/shopify_api_mixins.rb
+++ lib/shopify_api_mixins.rb
@@ -1,6 +1,20 @@
 require "shopify_api_mixins/version"
 
 module ShopifyApiMixins
+
+  class OldActiveResourceDependency < RuntimeError
+  end
+
+  unless ActiveResource::Connection.respond_to? :connection_class
+    raise OldActiveResourceDependency, <<-ERROR
+      Your version of activeresource is too old, and does not support the `connection_class` method. Recommend you upgrade to master. Add this to your Gemfile, replacing any existing activeresource dependency:
+
+      gem 'activeresource', git: 'https://github.com/rails/activeresource.git'
+
+      Then run `bundle update activeresource`.
+    ERROR
+  end
+
   require 'shopify_api_mixins/connection_mixins'
   ShopifyApi::Connection.singleton_class.prepend ConnectionMixins
 end
