f143d319aa3f3361313b07b0877f48488d53398d
Stefan Adamov <stefan.adamof@gmail.com>
2017-06-23 16:57:35 +0300

    Use Rails logger

--- lib/shopify_api_mixins/retry_on429.rb
+++ lib/shopify_api_mixins/retry_on429.rb
@@ -22,7 +22,7 @@ module ShopifyApiMixins
           when 429
             wait_time = e.response['Retry-After'].to_i
             wait_time = min_wait if wait_time < RetryOn429.min_wait
-            logger.info "Got a 429, will retry in #{wait_time} seconds"
+            Rails.logger.info "Got a 429, will retry in #{wait_time} seconds"
             sleep(wait_time)
             next
           else
--- lib/shopify_api_mixins/throttling.rb
+++ lib/shopify_api_mixins/throttling.rb
@@ -12,7 +12,7 @@ module ShopifyApiMixins
           if num_calls >= Throttling.max_calls
             @wait_until = Time.now + ((num_calls - Throttling.max_calls + 1).to_f/Throttling.leak_rate).seconds
           end
-          logger.debug("API Calls used: #{num_calls}/#{Throttling.max_calls}")
+          Rails.logger.debug("API Calls used: #{num_calls}/#{Throttling.max_calls}")
         end
       end
     end
@@ -21,7 +21,7 @@ module ShopifyApiMixins
       if @wait_until
         sleep_time = @wait_until - Time.now
         if sleep_time > 0
-          logger.info "waiting #{sleep_time} seconds to allow bucket to drain"
+          Rails.logger.info "waiting #{sleep_time} seconds to allow bucket to drain"
           sleep(sleep_time)
         end
       end
